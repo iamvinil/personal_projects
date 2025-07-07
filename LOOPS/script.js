e0c9455f316fc0ef94b3fabe9c40a242af099794
vinil <119056386+iamvinil@users.noreply.github.com>
2025-07-07 23:28:08 +0530

    Add files via upload

---
+++ LOOPS/script.js
@@ -0,0 +1,16 @@
+// WHILE //
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+//  DO WHILE //
+let j = 0;
+do {
+  console.log(j);
+  j++;
+} while (j < 5);
+// FOR //
+for (let k = 0; k < 10; k++) {
+  console.log(k);
+}
